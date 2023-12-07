429c78b274c9a93b04fa97de17ffa98a10719d80
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2023-12-07 11:52:26 -0500

    added writeToFile function

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const {Circle, Square, Triangle} = require('./lib.shapes');
 
+class Svg {
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
+
 const questions = [
     {
         type: 'input',
@@ -26,19 +43,12 @@ const questions = [
     },
 ];
 
-class Svg {
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render(){
-
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    }
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
-    }
+function writeToFile(fileName, data) {
+    console.log('Writing [' + data + '] to file [' + fileName + ']')
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('logo.svg has been generated!')
+    });
 }
